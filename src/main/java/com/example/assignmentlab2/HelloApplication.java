b25ca55e2da52c07ee8008cfc857116155461a23
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2024-09-13 23:47:01 -0400

    Added comments

--- src/main/java/com/example/assignmentlab2/HelloApplication.java
+++ src/main/java/com/example/assignmentlab2/HelloApplication.java
@@ -15,31 +15,42 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //Created a label for annual rate and set its position
         Label ir = new Label("Annual Interest Rate");
         ir.setLayoutY(50); ir.setLayoutX(10);
+        //Created a text field for the user to input the annual rate and set its position
         TextField intRate = new TextField();
         intRate.setLayoutX(130);
         intRate.setLayoutY(50);
+        //Created a label for number of years and set its position
         Label ny = new Label("Number of Years");
         ny.setLayoutY(100); ny.setLayoutX(10);
+        //Created a text field for the user to input the number of years and set its position
         TextField numYears = new TextField();
         numYears.setLayoutY(100);
         numYears.setLayoutX(130);
+        //Created a label for the loan amount and set its position
         Label la = new Label("Loan Amount");
         la.setLayoutY(150);la.setLayoutX(10);
+        //Created a text field for the user to input the loan amount and set its position
         TextField amt = new TextField();
         amt.setLayoutX(130);
         amt.setLayoutY(150);
+        //Created a label for monthly payment and set its position
         Label mp = new Label("Monthly Payment");
         mp.setLayoutY(200);mp.setLayoutX(10);
+        //Created a text field for the monthly pay to be shown
         TextField pay = new TextField();
         pay.setLayoutX(130);
         pay.setLayoutY(200);
+        //Created a label for total payment and set its position
         Label tp = new Label("Total Payment");
         tp.setLayoutY(250); tp.setLayoutX(10);
+        //Creates a text field for the total amount to be shown
         TextField total = new TextField();
         total.setLayoutX(130);
         total.setLayoutY(250);
+        //Created a button to calculate
         Button calc = new Button("Calculate");
         calc.setLayoutX(160);
         calc.setLayoutY(290);
@@ -48,13 +59,19 @@ public class HelloApplication extends Application {
         stage.setTitle("Loan Calculator");
         stage.setScene(scene);
         stage.show();
+        //The code needed to allow the calculate button to properly function
         calc.setOnAction(e -> {
+            //Calculates the interest rate and assigns it to rate
             double rate = Double.parseDouble(intRate.getText()) / 100 + 1;
+            //Assigns the numYears text field to the double years
             double years = Double.parseDouble(numYears.getText());
+            //Assigns the amt text field to the double amount
             double amount = Double.parseDouble(amt.getText());
-            //I wasn't sure how to get the values that were given in the picture so I hope this is fine
-            double totalAmount = rate * years * amount;
+            //Calculates the total amount that needs to be paid
+            double totalAmount = rate * years * amount; //I wasn't sure how to get the values that were given in the picture so I hope this is fine
+            //Calculates the monthly amount by dividing by the total number of months
             double monthlyPay = totalAmount / (years * 12);
+            //Rounds the two values and puts them into the proper text fields
             total.setText(String.valueOf(Math.round(totalAmount*100.0)/100.0));
             pay.setText(String.valueOf(Math.round(monthlyPay*100.0)/100.0));
 
